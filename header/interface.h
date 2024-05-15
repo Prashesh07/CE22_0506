c2c8f2d59dac25d29c4b3efdb9207f5cf6abd89e
Prashesh <aprashesh@gmail.com>
2024-05-15 05:15:02 +0100

    "first"

---
+++ header/interface.h
@@ -0,0 +1,12 @@
+#ifndef interface_h
+#define interface_h
+
+class BST
+{
+    virtual bool isEmpty() = 0;
+    virtual void addBST(int ) = 0;
+    virtual bool removeBST(int ) = 0;
+    virtual bool searchBST(int ) = 0; 
+};
+
+#endif
